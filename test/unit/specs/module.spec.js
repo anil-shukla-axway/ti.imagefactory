681333092a773fc23e6f6403caadc42bf0f3b6dd
Anil Shukla <40199172+anil-shukla-axway@users.noreply.github.com>
2020-05-04 17:56:25 +0530

    feat(ios): added unit test for all the functions.

--- test/unit/specs/module.spec.js
+++ test/unit/specs/module.spec.js
@@ -7,9 +7,23 @@ describe('ti.imagefactory', function () {
 
 	it('can be required', () => {
 		imagefactory = require('ti.imagefactory');
-
 		expect(imagefactory).toBeDefined();
 	});
+
+	beforeEach(() => {
+		var imageFilename = "flower.jpg";
+		const blob = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory + imageFilename).read();
+		var newBlob = imagefactory.imageWithAlpha(blob, {format: imagefactory.PNG });
+		newBlob = ImageFactory.imageWithTransparentBorder(blob, { borderSize: 10, format: ImageFactory.PNG });
+		newBlob = ImageFactory.imageAsCropped(blob, { width: 100, height: 100, x: 50, y: 50 });
+		newBlob = ImageFactory.imageWithRoundedCorner(blob, { borderSize: 4, cornerRadius: 8, format: ImageFactory.PNG });
+		newBlob = ImageFactory.imageAsThumbnail(blob, { size: 64, borderSize: 5, cornerRadius: 10, format: ImageFactory.PNG });
+		newBlob = ImageFactory.imageAsResized(blob, { width: 140, height: 140 });
+		newBlob = ImageFactory.imageTransform(blob,
+					{ type: ImageFactory.TRANSFORM_CROP, width: 200, height: 200 },
+					{ type: ImageFactory.TRANSFORM_ROUNDEDCORNER, borderSize: 6, cornerRadius: 20, format: ImageFactory.PNG }
+				);
+		});
 
 	describe('constants', () => {
 		describe('TRANSFORM_*', () => {
@@ -64,4 +78,43 @@ describe('ti.imagefactory', function () {
 			});
 		});
 	});
-});
+		describe ('hasAlpha' , () =>{
+				it('imgage should has an alpha layer',  () =>{
+					expect(imagefactory.hasAlpha(newBlob)).toEqual(true);
+				});
+			});
+			describe('imageWithAlpha',  () =>{
+				it('to returns a copy of the given image and check image should has an alpha layer ',  () =>{
+					expect(imagefactory.hasAlpha(newBlob)).toEqual(true);
+					expect(imagefactory.imageWithAlpha(newBlob)).toEqual(jasmine.any(Function));
+				});
+			});
+			describe('transparentBorderImage',  () =>{
+				it('to returns a copy of the image with a transparent border of the given size added around its edges',  () =>{
+					expect(imagefactory.hasAlpha(newBlob)).toEqual(true);
+					var borderSize = 5;
+					var rotateImage = imagefactory.transparentBorderImage(newBlob,borderSize);
+					expect(rotateImage).toEqual(newBlob);
+				});
+			});
+			describe('croppedImage',  () =>{
+				it('to returns a copy of this image that is cropped to the given bounds',  () =>{
+					let newImage = imagefactory.croppedImage(newBlob);
+					expect(newImage).toEqual(newBlob);
+				});
+			});
+			describe('rotateImage',  () =>{
+				it('to rotate the image context',  () =>{
+					var rotateImage = imagefactory.rotateImage(newBlob,degree)
+					var degree = 10.0;
+					expect(rotateImage).toEqual(newBlob);
+				});
+			});
+			describe('roundedCornerImage',  () =>{
+				it('to creates a copy of this image with rounded corners',  () =>{
+					var cornerSize = 8;
+					var borderSize = 4;
+					expect(imagefactory.roundedCornerImage(newBlob,cornerSize,borderSize)).toEqual(newBlob);
+				});
+			});
+})
